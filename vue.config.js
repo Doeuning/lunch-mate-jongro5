6a2b351396ecfcb1f8ce0189eaf5c26a888be19d
Doeun Kim <doeun.kim@emotion.co.kr>
2022-01-03 16:50:05 +0900

    회원가입 - validation

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,16 @@
 module.exports = {
   devServer: {
+    port: 8080,
     overlay: false,
+    disableHostCheck: true,
     proxy: {
-      "/": {
-        target: "https://www.juso.go.kr/addrlink/addrLinkApi.do",
-        pathRewrite: { "^/": "" },
+      "/address/api": {
+        target: process.env.VUE_ADDRESS_API_URL,
         changeOrigin: true,
-        secure: false,
       },
-      // "/async": {
-      //   target: "https://openapi.naver.com/v1/papago/n2mt",
-      //   pathRewrite: { "^/": "" },
-      //   changeOrigin: true,
-      //   secure: false,
-      // },
-      // "/address": {
-      //   target: "https://doeuning.github.io/lunch-mate/",
-      // },
-    },
-    devServer: {
-      port: 8080,
-      overlay: false,
-      disableHostCheck: true,
-      // host: 'localhost',
-      proxy: {
-        "/address/api": {
-          target: process.env.VUE_APP_API_URL,
-          changeOrigin: true,
-        },
-
-        // todo : test proxy 삭제 필요
-        "/b2b": {
-          target: process.env.VUE_APP_API_URL,
-          changeOrigin: true,
-        },
+      "/api": {
+        target: process.env.VUE_APP_API_URL,
+        changeOrigin: true,
       },
     },
   },
